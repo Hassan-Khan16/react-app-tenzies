ac2c2957eb64f178564509bd263a3bfb31a6d3a0
Hassan Khan <hassankpisces@gmail.com>
2024-06-24 04:52:29 +0500

    deploy to gitpages 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-notes-tenzies/",
   plugins: [react()],
-  base: "/react-notes-tenzies/"
 })
